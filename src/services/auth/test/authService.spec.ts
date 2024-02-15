3f23ecc9ec05c9bf9db988b7777f78876a79553f
Nkoa Christophe <nkoachristophe@gmail.com>
2024-02-15 21:08:07 +0100

    add tests to services

--- src/services/auth/test/authService.spec.ts
+++ src/services/auth/test/authService.spec.ts
@@ -1,6 +1,6 @@
 import AuthService from '../authService'
 
-describe('Test Delivery service', () => {
+describe('Test Authentification service', () => {
     const authService = new AuthService();
     const test_token: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImlhdCI6MTcwODAyNzI0NywiZXhwIjoxNzA4NjMyMDQ3fQ.ul6TjE6lg-95gQPwou_VciX5Tawbl39lVxCDhCBAzqU"
     const email: string = "admin@gmail.com";
